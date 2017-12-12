f6cef7290e3a4cd7430e905cd9f36e638da61d28
yangeric7 <yangeric7@gmail.com>
2017-12-11 19:01:56 -0600

    finished day 9 puzzles

--- solutions/day9/day9.py
+++ solutions/day9/day9.py
@@ -1,11 +1,43 @@
 
 
-def part1(input):
-	return -1
+def answer(input_str):
+	total_score = 0
+	garbage_count = 0
+	score_stack = [0]
 
+	inGarbage = False
+	skipChar = False
+
+	for i in range(0,len(input_str)):
+		char = input_str[i]
+
+		if skipChar:
+			skipChar = False
+			continue
+		elif char == '!':
+			skipChar = True
+			continue
+
+		if inGarbage:
+			if char == '>':
+				inGarbage = False
+			else:
+				garbage_count += 1
+		else:
+			if char == '<':
+				inGarbage = True
+			elif char == '{':
+				score_stack.append((score_stack[-1] + 1))
+			elif char == '}':
+				total_score += score_stack.pop()
+
+	return total_score, garbage_count
 def main():
-	with open('input.txt','r') as input_file:
+	input_string = open('input.txt','r').read()
 
+	print('Problem 1: ' + str(answer(input_string)[0]))
+	print('Problem 2: ' + str(answer(input_string)[1]))
+	#print(part1('{{<a!>},{<a!>},{<a!>},{<ab>}}'))
 
 if __name__ == '__main__':
 	main()
