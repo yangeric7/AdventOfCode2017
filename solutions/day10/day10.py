caef298e30477488edef6cf3ffbfee5ae8d0fd8b
yangeric7 <yangeric7@gmail.com>
2017-12-12 18:45:25 -0600

    updated day10 solution. mistakes were that forgot to trim off new line character and also white space matters for part 2 (for any test purposes)

--- solutions/day10/day10.py
+++ solutions/day10/day10.py
@@ -8,8 +8,6 @@ def decrypt(input_file,input_list):
 		skipSize += 1
 	return input_list[0] * input_list[1]
 
-
-
 def knothash(length,currentPos, skipSize, curList):
 	reverse_list = reverse(curList,currentPos, length)
 	curIndex = currentPos
@@ -36,48 +34,43 @@ def reverse(input_list, start, length):
 		reverse_list = input_list[start:(start+length)][::-1]
 	return reverse_list
 
-
-
 def main():
-	string = '1,2,3'.replace(' ','')
 	with open('input.txt','r') as input_text:
-		pass
-		#input_nums= [int(num) for num in input_text.read().strip().split(',')]
-		#input_len = [ord(char) for char in input_text.read()]
+		line = input_text.readline()
+		input_nums= [int(num) for num in line.strip().split(',')]
+		input_len = [ord(char) for char in line.strip()]
 
-	input_len = [ord(string[char]) for char in range(0,len(string))]
 	lengths = input_len + [17,31,73,47,23]
 
 	input_list = []
-	for i in range(0,256):
+	for i in range(256):
 		input_list.append(i)
 	
-	#print('Problem 1: ' + str(decrypt(input_nums, list(input_list))))
+	print('Problem 1: ' + str(decrypt(input_nums, list(input_list))))
 
 	second_list = list(input_list)
 	skipSize = 0
 	currentPos = 0
 
-	for i in range(0,64):
+	for i in range(64):
 		for length in lengths:
 			currentPos = knothash(length, currentPos, skipSize, second_list)
 			skipSize += 1
 
 	dense_hash = []
-	for i in range(0,16):
-		sparse_hash = second_list[16*i:16*i+16]
+	for i in range(0,len(second_list),16):
+		sparse_hash = second_list[i:i+16]
 		output = reduce(lambda x,y: x^y, sparse_hash)
 		dense_hash.append(output)
 
 	hexdecimal = ''
 	for num in dense_hash:
 		hexhash = hex(num).replace('0x','')
-		print(hexhash)
 		if len(hexhash) == 1:
 			hexhash = '0' + hexhash
 		hexdecimal += hexhash
 
-	print(hexdecimal)
+	print('Problem 2: '+ str(hexdecimal))
 
 if __name__ == '__main__':
 	main()
